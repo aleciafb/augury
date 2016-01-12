a6cafe207e6c290fc68feeb25836f1bdb1ce9ef6
Alecia Bester <alecia.bester@gmail.com>
2016-01-12 16:33:17 +0200

    Fix create ticket migration

--- db/migrate/20160112142021_create_tickets.rb
+++ db/migrate/20160112142021_create_tickets.rb
@@ -3,8 +3,8 @@ class CreateTickets < ActiveRecord::Migration
     create_table :tickets do |t|
       t.string :name
       t.text :description
-      t.date_time :start_date
-      t.date_time :end_date
+      t.datetime :start_date
+      t.datetime :end_date
       t.references :state, index: true, foreign_key: true
 
       t.timestamps null: false
