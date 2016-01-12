e55a6a18af8d8f5de929ca6d458395e87e508c3d
Alecia Bester <alecia.bester@gmail.com>
2016-01-12 16:22:52 +0200

    Bug model

---
+++ app/models/bug.rb
@@ -0,0 +1,3 @@
+class Bug < ActiveRecord::Base
+  belongs_to :project
+end
---
+++ db/migrate/20160112142244_create_bugs.rb
@@ -0,0 +1,9 @@
+class CreateBugs < ActiveRecord::Migration
+  def change
+    create_table :bugs do |t|
+      t.references :project, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
